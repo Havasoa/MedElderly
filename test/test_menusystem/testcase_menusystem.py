90df8864831ccad9af33f22e45bb68bf8cb75eaa
o92design <o92design@gmail.com>
2018-11-11 22:40:46 +0100

    Story5 - Menusystem   * Skapade en basklass, Menu, som vi kan utöka allteftersom.   * Finns ett unittest för denna där vi bara testar vilket värde vi får     ut från action

--- test/test_menusystem/testcase_menusystem.py
+++ test/test_menusystem/testcase_menusystem.py
@@ -1,30 +1,32 @@
 import unittest
 from unittest import mock
+
 from src.menusystem.Menu import Menu
 
 
-choises = (choise for choise in [0, 1])
 
-def mock_input():
+choises = (choise for choise in [5, 0, 1])
+
+def mock_input(prompt):
     return next(choises)
 
 class testcase_Menusystem(unittest.TestCase):
 
-    def test_GoBack(self):
+    def test_ReturnChoise(self):
         mainMenu = Menu("Huvudmeny",
                         "Detta är ett program för att hålla koll på äldres mediciner.",
                         "Var god skriv in Personnummer",
                         ["Logga in"])
         mainMenu.actions[0] = "Avsluta"
 
-        with mock.patch('builtins.input', return_value=mock_input()):
+        ## Vi testar att först skriva fel och sedan skriva rätt
+        with mock.patch('builtins.input', mock_input):
             returnChoise = mainMenu.action()
             self.assertEqual(0, returnChoise, "The menu did not go back when we chose 0")
 
-        with mock.patch('builtins.input', return_value=mock_input()):
+        with mock.patch('builtins.input', mock_input):
             returnChoise = mainMenu.action()
-            self.assertEqual(1, returnChoise, "The menu did not go continue when we chose 1")
-
+            self.assertEqual(1, returnChoise, "The menu did not go back when we chose 1")
 
 if __name__ == '__main__':
     unittest.main()
